21b88db0c3d4586731e54758096ef62e67b15516
nhan le <lehoainhan2707>
2023-06-10 20:07:58 +0700

    update login and register

--- src/redux/slices/Product.js
+++ src/redux/slices/Product.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import { getLocalStorage, saveLocalStogare } from '../../utils';
+import { getLocalStorage, saveLocalStogare } from '../../utils/LocalStorage/LocalStorage';
 
 // const listProductCart = JSON.parse(localStorage.getItem('listProductCart')) || [];
 const listProductCart = getLocalStorage('listProductCart') || []
